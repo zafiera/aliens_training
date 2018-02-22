42c4ed20ff43bfba13f4aeb3a6a86f51c5f35827
zafiera <30795388+zafiera@users.noreply.github.com>
2018-02-22 14:18:49 +0200

    22/02/18

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -6,30 +6,67 @@ GAME RULES:
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
 
-function newGame() {
-    
-    document.querySelector('.dice').style.display = 'none';
+var determinePlayer = 0;
+var activePlayer = 0
 
-    document.getElementById('score-0').textContent = '0';
-    document.getElementById('score-1').textContent = '0';
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-    document.getElementById('name-0').textContent = 'Player 1';
-    document.getElementById('name-1').textContent = 'Player 2';
+function newGame () {
+  document.querySelector('.dice').style.display = 'none'
+
+  document.getElementById('score-0').textContent = '0'
+  document.getElementById('score-1').textContent = '0'
+  document.getElementById('current-0').textContent = '0'
+  document.getElementById('current-1').textContent = '0'
+  document.getElementById('name-0').textContent = 'Player 1'
+  document.getElementById('name-1').textContent = 'Player 2'
 }
 
-function rollDice(){
-    var pictures = ['dice-1.png', 'dice-2.png', 'dice-3.png', 'dice-4.png', 'dice-4.png', 'dice-5.png', 'dice-6.png'];
-   var dice = document.getElementById('dice');
-    document.querySelector('.dice').style.display = 'block';
+function rollDice () {
+  var pictures = ['dice-1.png', 'dice-2.png', 'dice-3.png', 'dice-4.png',
+    'dice-5.png', 'dice-6.png']
+  var dice = document.getElementById('dice')
 
-    var rollingDice = Math.floor(Math.random()*6)+1;
-    console.log(rollingDice);
+  document.querySelector('.dice').style.display = 'block'
 
-    document.dice.src=pictures[rollingDice-1];
-    console.log(pictures[rollingDice]);
-    // dice.querySelector('.dice')
+  var rollingDice = Math.floor(Math.random() * 6)
+  console.log(rollingDice + 1)
+
+  document.dice.src = pictures[rollingDice]
+  console.log(pictures[rollingDice])
+
+  if (rollingDice >= 1) {
+    var score = rollingDice + 1
+    var getScore = parseInt(document.getElementById('current-' + activePlayer).textContent)
+    var currentScore = score + getScore
+    document.querySelector('#current-' + activePlayer).innerHTML = currentScore
+    console.log(getScore)
+  } else {
+    document.querySelector('#current-' + activePlayer).innerHTML = '0'
+    changePlayer()
+  }
+}
+
+function changePlayer () {
+    determinePlayer++
+  if (determinePlayer % 2 == 0) {
+    activePlayer = 0
+  }else {
+    activePlayer = 1
+  }
+  document.querySelector('.player-1-panel').classList.toggle('active')
+  document.querySelector('.player-0-panel').classList.toggle('active')
+
+
+    document.querySelector('#current-' + activePlayer).innerHTML = '0'
+    console.log('player' + activePlayer)
+
+}
+
+function hold () {
+  var getSavedScore = parseInt(document.getElementById('score-' + activePlayer).textContent)
+  var getScore = parseInt(document.getElementById('current-' + activePlayer).textContent)
+  var score = document.getElementById('score-' + activePlayer)
+  score.innerHTML = getScore+getSavedScore;
+  changePlayer()
 }
