864baf23c12196f683beabaf9d1a1dd1a759b0fd
zafiera <30795388+zafiera@users.noreply.github.com>
2018-03-12 14:01:03 +0200

    ES6 call api

--- es6-tutorial-master/app/index.js
+++ es6-tutorial-master/app/index.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      city: "San Francisco"
+      city: "Cape Town"
     };
   }
 
@@ -19,10 +19,12 @@ class App extends Component {
     fetch(
       `http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}`
     ).then(response => response.json())
-    .then(json => console.log(json));
+    .then(json => {
+      this.setState({description: json.weather[0].des})
+    });
   }
   render() {
-    return <div> ReactJS and JSX in action</div>;
+    return <div><h1> Weather Report for {this.state.city}! </h1></div> ;
   }
 }
 
