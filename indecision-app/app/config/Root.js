5a518acc53f2b01c5160e4b496baff1d48e0acde
Jethro Flanagan <jethro@cowboyaliens.com>
2018-03-26 12:03:16 +0200

    New boilerplate app structure

---
+++ indecision-app/app/config/Root.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import App from '../components/App';
+
+const Root = () => {
+  return (
+    <App />
+    // <Router>
+    //   <Switch>
+    //     <Route path="/" component={App} exact />
+    //   </Switch>
+    // </Router>
+  );
+};
+
+export default Root;
+
