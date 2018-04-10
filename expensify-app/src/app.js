6437e7eb17637e62ded9c282747acf0249992968
zafiera <30795388+zafiera@users.noreply.github.com>
2018-04-10 10:38:07 +0200

    organising expensify

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-import AppRouter from './routers/AppRouter';
+import React from "react";
+import ReactDOM from "react-dom";
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+const store = configureStore();
+
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+  });
+
+store.dispatch(addExpense({ description: "water bill" }));
+store.dispatch(addExpense({ description: "gas bill"}));
+store.dispatch(setTextFilter("bill"));
+store.dispatch(setTextFilter("water"));
 
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById("app"));
